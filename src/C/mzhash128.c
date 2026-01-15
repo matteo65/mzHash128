83dcc5f75683e88514c550dc2cb775385895d095
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2026-01-15 13:19:06 +0100

    Refactor mzhash128 functions for simplicity

--- src/C/mzhash128.c
+++ src/C/mzhash128.c
@@ -15,23 +15,7 @@
 
 #include "mzhash128.h"
 
-void mzhash128(const char* data, size_t length, uint64_t seed, uint64_t* output)
-{
-	const int8_t *bytes = (const int8_t*)data;
-	uint64_t hash0 = 0xB04C2438F4F7D8D1uLL ^ seed;
-	uint64_t hash1 = 0xA3B8FD0DF0836C0DuLL ^ seed;
-		
-	while(length--) {
-		uint64_t h = 0xD76F648260B0F9FDuLL * (*bytes ^ (hash0 << 8) ^ (hash0 >> 8));
-		hash0 = 0xD1DA2131A0C25299uLL * (*bytes++ ^ (hash1 << 8) ^ (hash1 >> 8));
-		hash1 = h;
-	}
-	
-	output[0] = hash0;
-	output[1] = hash1;
-}
-
-void mzhash128_noseed(const char* data, size_t length, uint64_t* output)
+void mzhash128(const void* data, size_t length, uint64_t* output)
 {
 	const int8_t *bytes = (const int8_t*)data;
 	uint64_t hash0 = 0xB04C2438F4F7D8D1uLL;
@@ -47,22 +31,7 @@ void mzhash128_noseed(const char* data, size_t length, uint64_t* output)
 	output[1] = hash1;
 }
 
-void mzhash128_str(const char* str, uint64_t seed, uint64_t* output)
-{
-	uint64_t hash0 = 0xB04C2438F4F7D8D1uLL ^ seed;
-	uint64_t hash1 = 0xA3B8FD0DF0836C0DuLL ^ seed;
-		
-	while(*str) {
-		uint64_t h = 0xD76F648260B0F9FDuLL * (*str ^ (hash0 << 8) ^ (hash0 >> 8));
-		hash0 = 0xD1DA2131A0C25299uLL * (*str++ ^ (hash1 << 8) ^ (hash1 >> 8));
-		hash1 = h;
-	}
-	
-	output[0] = hash0;
-	output[1] = hash1;
-}
-
-void mzhash128_str_noseed(const char* str, uint64_t* output)
+void mzhash128_str(const char* str, uint64_t* output)
 {
 	uint64_t hash0 = 0xB04C2438F4F7D8D1uLL;
 	uint64_t hash1 = 0xA3B8FD0DF0836C0DuLL;
@@ -76,3 +45,4 @@ void mzhash128_str_noseed(const char* str, uint64_t* output)
 	output[0] = hash0;
 	output[1] = hash1;
 }
+
