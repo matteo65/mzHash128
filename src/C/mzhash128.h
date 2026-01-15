1ff63d70dc68949f7fc43724a9275c36b8450650
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2026-01-15 13:18:13 +0100

    Refactor mzhash128.h to simplify function signatures

--- src/C/mzhash128.h
+++ src/C/mzhash128.h
@@ -8,15 +8,12 @@
 extern "C" {
 #endif
 
-#define MZ_HASH_128_DEFAULT_SEED		0;
-
-void mzhash128(const char* data, size_t length, uint64_t seed, uint64_t* output);
-void mzhash128_noseed(const char* data, size_t length, uint64_t* output);
-void mzhash128_str(const char* str, uint64_t seed, uint64_t* output);
-void mzhash128_str_noseed(const char* str, uint64_t* output);
+void mzhash128(const void* data, size_t length, uint64_t* output);
+void mzhash128_str(const char* str, uint64_t* output);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
+
